929e12a6a7077e6a21116912784f68da2cd89769
ZihanXX <zihanxxie@gmail.com>
2018-01-02 19:13:47 -0800

    113

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map( igKey => {
+      // console.log([...Array(props.ingredients[igKey])]);
+      // if `salad: 2`, then the array is: [undefined, undefined]
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        // console.log(igKey);
+        // console.log(i);
+        return <BurgerIngredient key={igKey + i} type={igKey} />
+      });
+    });
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom' />
     </div>
   );
