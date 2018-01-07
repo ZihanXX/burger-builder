35b7770960142d007a41fab33c4a198fb00f1232
ZihanXX <zihanxxie@gmail.com>
2018-01-06 16:12:21 -0800

    141

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,39 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return <li key={igKey}>
-        <span style={{textTransform: 'capitalize'}}>{igKey}</span>:
-        {props.ingredients[igKey]}</li>
-    });
-  return (
-    <Aux>
+// This could be a functional component
+class OrderSummary extends Component {
+
+  componentWillUpdate() {
+    console.log("OrderSummary will update.");
+  }
+
+  render() {
+    const
+    ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>:
+          {this.props.ingredients[igKey]}</li>
+      });
+
+    return(
+      <Aux>
       <h3>Your Order:</h3>
       <p>Delicious burger with following ingredients:</p>
       <ul>
         {ingredientSummary}
       </ul>
-      <p>Total Price: <strong>{props.totalPrice.toFixed(2)}</strong></p>
+      <p>Total Price: <strong>{this.props.totalPrice.toFixed(2)}</strong></p>
       <p>Continue to Checkout? </p>
-      <Button onClick={props.purchaseCanceled} btnType='Danger'>CANCEL</Button>
-      <Button onClick={props.purchaseContinue} btnType='Success'>CONTINUE</Button>
-    </Aux>
-  )
+      <Button onClick={this.props.purchaseCanceled} btnType='Danger'>CANCEL</Button>
+      <Button onClick={this.props.purchaseContinue} btnType='Success'>CONTINUE</Button>
+      </Aux>
+    );
+  }
 }
 
 
-export default orderSummary;
+export default OrderSummary;
